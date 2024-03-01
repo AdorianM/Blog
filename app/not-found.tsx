3719c9e7fdd7b2a90225e43a8e9fc20f2b8cdaeb
Adorian Mititean <Adorian.Mititean@example.com>
2024-03-01 22:21:35 +0200

    Not Found page

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/Container";
+
+const NotFound = () => {
+    return (
+        <Container>
+            <div className="flex items-center justify-center h-screen text-center">
+                <h1>404 - This page was eaten by a camel 🐫</h1>
+            </div>
+        </Container>
+    );
+}
+
+export default NotFound;
