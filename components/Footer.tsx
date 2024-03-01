6ca6bed5d32256effda0abb1ee1bc26559246f03
Adorian Mititean <Adorian.Mititean@example.com>
2024-03-01 22:40:55 +0200

    social media

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import { ContentMetadata, NewsMetadata } from "@/types/types"
 import ContentLinkList from "@/components/ContentLinkList"
 import getMetadata from "@/lib/mdx"
 import Link from "next/link"
+import Image from "next/image"
 
 const Footer = () => {
     const gamesMetadata = getMetadata<ContentMetadata>("content/games/")
@@ -22,21 +23,25 @@ const Footer = () => {
                 </div>
                 <div className="footer-info">
                     <p className="unimportant">© 2024 Future Camel</p>
-                    <div className="flex space-x-2">
+                    <div className="flex space-x-2 align-middle">
                         <Link href="">
                             Privacy Policy
                         </Link>
                         <Link href="">
                             Terms of Service
                         </Link>
-                        <Link href="https://twitter.com/futurecamel">
-                            {/* <img src="/twitter.svg" alt="Twitter" /> */}
+                        
+                        <Link href="https://olddews.itch.io/vampire-solitaire">
+                            <Image src="/images/social/itchio-logo-white.png" alt="Itch Io" width={20} height={20} />
+                        </Link>
+                        <Link href="https://x.com/futurecamel">
+                            <Image src="/images/social/x-logo-white.png" alt="X" width={20} height={20} />
                         </Link>
                         <Link href="https://facebook.com/futurecamel">
-                            {/* <img src="/facebook.svg" alt="Facebook" /> */}
+                            <Image src="/images/social/facebook-logo-white.png" alt="Facebook" width={20} height={20} />
                         </Link>
                         <Link href="https://instagram.com/futurecamel">
-                            {/* <img src="/instagram.svg" alt="Instagram" /> */}
+                            <Image src="/images/social/instagram-logo-white.png" alt="Instagram" width={20} height={20} />
                         </Link>
                     </div>
                 </div>
